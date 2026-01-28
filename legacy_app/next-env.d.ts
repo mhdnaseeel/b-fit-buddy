ee11f2e6e0de05fcb86c93bb0414a6876174ef5c
mhdnaseeel <mhdnaseel521@gmail.com>
2026-01-29 00:50:31 +0530

    Implement B Fit Buddy branding and integrate assets

--- legacy_app/next-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-
-declare var API_KEY: string;
-
-declare module '*.json' {
-  const value: any;
-  export default value;
-}
